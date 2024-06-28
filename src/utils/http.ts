414689e436774bdcbbc6203f6dc2563e332f4b80
yeeeeeeeop <yeop0916@gmail.com>
2024-06-28 18:28:32 +0900

    ✨ [#3] FEAT   : http 인스턴스, storage 유틸함수 생성

---
+++ src/utils/http.ts
@@ -0,0 +1,110 @@
+import axios, {
+  AxiosRequestConfig,
+  RawAxiosResponseHeaders,
+  AxiosResponseHeaders,
+  InternalAxiosRequestConfig,
+  AxiosResponse,
+} from 'axios';
+
+import {getStorage} from './storage';
+import {SERVER_URL} from '@env';
+
+type ResponseCode = string;
+
+/**
+ * @type T: 응답 데이터 DTO
+ * @type D: RequestDTO
+ */
+export interface APIResponse<T, D = any> {
+  isSuccess: boolean;
+  code: ResponseCode;
+  message: string;
+  result?: T;
+  headers: RawAxiosResponseHeaders | AxiosResponseHeaders;
+  config: InternalAxiosRequestConfig<D>;
+  request?: any;
+}
+
+const axiosInstance = axios.create({
+  baseURL: SERVER_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use(async config => {
+  const accessToken = await getStorage('accessToken');
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
+
+/**
+ * @TODO response 인터셉터 생성 필요(토큰 만료 관련)
+ */
+
+function convertToResponseDTO<T>(response: AxiosResponse): APIResponse<T> {
+  return {
+    isSuccess: response.data.isSuccess,
+    code: response.data.code,
+    message: response.data.message,
+    result: response.data.result,
+    headers: response.headers,
+    config: response.config,
+    request: response.request,
+  };
+}
+
+const http = {
+  get: async <T>(
+    url: string,
+    config?: AxiosRequestConfig,
+  ): Promise<APIResponse<T>> => {
+    const response = await axiosInstance.get(url, config);
+
+    return convertToResponseDTO(response);
+  },
+
+  post: async <T>(
+    url: string,
+    requestBody?: any,
+    config?: AxiosRequestConfig,
+  ): Promise<APIResponse<T>> => {
+    const response = await axiosInstance.post(url, requestBody, config);
+
+    return convertToResponseDTO(response);
+  },
+
+  patch: async <T>(
+    url: string,
+    requestBody?: any,
+    config?: AxiosRequestConfig,
+  ): Promise<APIResponse<T>> => {
+    const response = await axiosInstance.patch(url, requestBody, config);
+
+    return convertToResponseDTO(response);
+  },
+
+  put: async <T>(
+    url: string,
+    requestBody?: any,
+    config?: AxiosRequestConfig,
+  ): Promise<APIResponse<T>> => {
+    const response = await axiosInstance.put(url, requestBody, config);
+
+    return convertToResponseDTO(response);
+  },
+
+  delete: async <T>(
+    url: string,
+    requestBody?: any,
+  ): Promise<APIResponse<T>> => {
+    const response = await axiosInstance.delete(url, requestBody);
+
+    return convertToResponseDTO(response);
+  },
+};
+
+export default http;
