ecc7d41bd3259966db08ed7a3846fdc06c0f39eb
yeeeeeeeop <yeop0916@gmail.com>
2024-06-29 15:07:59 +0900

    ✨ [#5] FEAT   : FCM 백그라운드 알림 구현 완료

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import messaging from '@react-native-firebase/messaging';
 
 import RootStack from './src/screens/RootStack';
+import {requestUserPermission} from './src/utils/permission';
 
 export default function App() {
+  async function fetchToken() {
+    const token = await messaging().getToken();
+
+    console.log('fcm token:', token);
+  }
+
+  requestUserPermission();
+  fetchToken();
+
   return (
     <NavigationContainer>
       <RootStack />
