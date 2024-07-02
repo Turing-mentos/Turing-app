fc3446a61ba139355f262b1c83b6fd586ec21342
yeeeeeeeop <yeop0916@gmail.com>
2024-07-03 01:31:43 +0900

    ✨ [#5] FEAT   : 색상 theme 설정

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import messaging from '@react-native-firebase/messaging';
+import {ThemeProvider} from '@emotion/react';
 
 import RootStack from './src/screens/RootStack';
 import {requestUserPermission} from './src/utils/permission';
+import theme from './src/styles/theme';
 
 export default function App() {
   async function fetchToken() {
@@ -16,8 +18,10 @@ export default function App() {
   fetchToken();
 
   return (
-    <NavigationContainer>
-      <RootStack />
-    </NavigationContainer>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
