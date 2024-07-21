c2ae0d49bee491a7222d2b70e478fa055a559829
yeeeeeeeop <yeop0916@gmail.com>
2024-07-21 16:33:11 +0900

    ✨ [#16] FEAT   : 챗박스 내 Indicator 구현

--- src/components/report/ChatComponents.tsx
+++ src/components/report/ChatComponents.tsx
@@ -1,41 +1,97 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styled from '@emotion/native';
+import {Animated} from 'react-native';
+import Indicator from './Indicator';
 
-export const ChatBox = styled.View`
+const AnimatedChatBoxStyled = Animated.createAnimatedComponent(styled.View`
   align-self: flex-start;
   gap: 8px;
   padding: 16px 20px;
   border-radius: 0px 20px 20px 20px;
   background-color: ${props => props.theme.color.grey[100]};
-`;
+`);
 
-export const MyChatBox = styled.View`
+export const ChatBox = ({
+  children,
+  loading = false,
+}: {
+  children: any;
+  loading?: boolean;
+}) => {
+  const translateY = new Animated.Value(30);
+  const opacity = new Animated.Value(0);
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.spring(translateY, {
+        toValue: 0,
+        useNativeDriver: true,
+      }),
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, []);
+
+  return (
+    <AnimatedChatBoxStyled style={{transform: [{translateY}], opacity}}>
+      {loading && <Indicator />}
+      {!loading && children}
+    </AnimatedChatBoxStyled>
+  );
+};
+
+const AnimatedMyChatBox = Animated.createAnimatedComponent(styled.View`
   align-self: flex-end;
+  max-width: 280px;
   padding: 12px 16px;
   border-radius: 20px 0px 20px 20px;
   background-color: ${props => props.theme.color.grey[700]};
-`;
+`);
+
+export const MyChatBox = ({children}: {children: any}) => {
+  const translateY = new Animated.Value(30);
+  const opacity = new Animated.Value(0);
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.spring(translateY, {
+        toValue: 0,
+        useNativeDriver: true,
+      }),
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, []);
+
+  return (
+    <AnimatedMyChatBox style={{transform: [{translateY}], opacity}}>
+      {children}
+    </AnimatedMyChatBox>
+  );
+};
 
 export const Text = styled.Text`
   color: ${props => props.theme.color.BTN900};
-
-  /* Text/M16 */
   font-family: Pretendard;
   font-size: 16px;
   font-style: normal;
   font-weight: 500;
-  line-height: 24px; /* 24px */
+  line-height: 24px;
 `;
 
 export const MyText = styled.Text`
   color: ${props => props.theme.color.grey[100]};
-
-  /* Text/M16 */
   font-family: Pretendard;
   font-size: 16px;
   font-style: normal;
   font-weight: 500;
-  line-height: 24px; /* 24px */
+  line-height: 24px;
 `;
 
 const AppIconImage = styled.Image`
