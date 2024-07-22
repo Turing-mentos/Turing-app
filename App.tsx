92c1ecab198b510c3fa6251770428f5ff9366eb8
yeeeeeeeop <yeop0916@gmail.com>
2024-07-23 01:01:23 +0900

    ✨ [#18] FEAT   : fcm 토큰 함수 분리

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import messaging from '@react-native-firebase/messaging';
 import {ThemeProvider} from '@emotion/react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {SimpleSheetProvider} from 'react-native-simple-sheet';
@@ -11,14 +10,7 @@ import {requestUserPermission} from './src/utils/permission';
 import theme from './src/styles/theme';
 
 export default function App() {
-  async function fetchToken() {
-    const token = await messaging().getToken();
-
-    console.log('fcm token:', token);
-  }
-
   requestUserPermission();
-  fetchToken();
 
   return (
     <GestureHandlerRootView style={{flex: 1}}>
