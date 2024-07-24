a78c21edb2b3210df10f17d115b8f3682f63fae5
yeeeeeeeop <yeop0916@gmail.com>
2024-07-25 00:57:21 +0900

    ✨ [#19] FEAT   : 최초 접근 시 알림 설정

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -4,12 +4,51 @@ import styled from '@emotion/native';
 import useUserStore from '../../store/useUserStore';
 import NoStudyRoom from './NoStudyRoom';
 import HomeInnerHeader from './HomeInnerHeader';
+import {getStorage, setStorage} from '../../utils/storage';
+import {NotificationAPI} from '../../api/notification';
+import {SimpleSheet, useSimpleSheet} from 'react-native-simple-sheet';
+import InitNotificationModalContent from './InitNotificationModalContent';
 
 export default function HomePage() {
   const user = useUserStore(state => state.user);
+  const sheet = useSimpleSheet();
+
   const [headerState, setHeaderState] = useState(0);
 
-  useEffect(() => {}, []);
+  const handleInitNotificationSetting = async (enabled: boolean) => {
+    try {
+      await NotificationAPI.initNotificationSetting(enabled);
+      await setStorage('visited', true);
+    } catch (err) {
+      console.log('handleInitNotificationSetting() err:', err);
+    }
+  };
+
+  useEffect(() => {
+    const initNotificationSetting = async () => {
+      const visited = await getStorage('visited');
+
+      if (!visited) {
+        // 모달 오픈
+        sheet.open(props => (
+          <SimpleSheet {...props} dismissible={false}>
+            <InitNotificationModalContent
+              onConfirm={async () => {
+                await handleInitNotificationSetting(true);
+                props.close();
+              }}
+              onCancel={async () => {
+                await handleInitNotificationSetting(false);
+                props.close();
+              }}
+            />
+          </SimpleSheet>
+        ));
+      }
+    };
+
+    initNotificationSetting();
+  }, []);
 
   return (
     <Container>
