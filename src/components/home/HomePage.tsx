39fa74b973b3ff4b14b961765f683fd060256146
yeeeeeeeop <yeop0916@gmail.com>
2024-07-25 23:01:43 +0900

    ✨ [#19] FEAT   : 홈 화면 임시

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -1,5 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
+import {useFocusEffect} from '@react-navigation/native';
 import styled from '@emotion/native';
+import {format} from 'date-fns';
 
 import useUserStore from '../../store/useUserStore';
 import NoStudyRoom from './NoStudyRoom';
@@ -8,12 +10,27 @@ import {getStorage, setStorage} from '../../utils/storage';
 import {NotificationAPI} from '../../api/notification';
 import {SimpleSheet, useSimpleSheet} from 'react-native-simple-sheet';
 import InitNotificationModalContent from './InitNotificationModalContent';
+import {requestUserPermission} from '../../utils/permission';
+import {HomeAPI, Schedule} from '../../api/home';
+import {StudyRoomAPI} from '../../api/studyRoom';
+import {groupSchedulesByDate} from '../../utils/time';
 
 export default function HomePage() {
   const user = useUserStore(state => state.user);
   const sheet = useSimpleSheet();
 
   const [headerState, setHeaderState] = useState(0);
+  const [schedules, setSchedules] = useState<Schedule[]>([]);
+  const [studyRoomIds, setStudyRoomIds] = useState<number[]>([]);
+
+  console.log('studyRoomIds:', studyRoomIds);
+  const today = format(new Date(), 'yyyy-MM-dd');
+  const dateSchedules = groupSchedulesByDate(schedules);
+
+  console.log('dateSchedules:', dateSchedules);
+  const todaySchedule = dateSchedules[today];
+
+  console.log('todaySchedule:', todaySchedule);
 
   const handleInitNotificationSetting = async (enabled: boolean) => {
     try {
@@ -35,6 +52,7 @@ export default function HomePage() {
             <InitNotificationModalContent
               onConfirm={async () => {
                 await handleInitNotificationSetting(true);
+                await requestUserPermission();
                 props.close();
               }}
               onCancel={async () => {
@@ -48,7 +66,39 @@ export default function HomePage() {
     };
 
     initNotificationSetting();
-  }, []);
+  }, [sheet]);
+
+  useFocusEffect(
+    useCallback(() => {
+      const fetchSchedules = async () => {
+        try {
+          const {data: studyRooms} =
+            await StudyRoomAPI.getStudyRoomsInProgress();
+
+          if (!studyRooms) {
+            throw new Error('studyRooms do not exist');
+          }
+
+          const fetchedStudyRoomIds = studyRooms.map(studyRoom => studyRoom.id);
+          setStudyRoomIds(fetchedStudyRoomIds.sort((a, b) => a - b));
+          const {data: fetchedSchedules} = await HomeAPI.getWeeklySchedule(
+            today,
+            fetchedStudyRoomIds,
+          );
+
+          if (!fetchedSchedules) {
+            throw new Error('fetchedSchedules do not exist');
+          }
+
+          setSchedules(fetchedSchedules);
+        } catch (err) {
+          console.log('schedule fetch error:', err);
+        }
+      };
+
+      fetchSchedules();
+    }, []),
+  );
 
   return (
     <Container>
